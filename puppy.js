711c55e818b503aeffe707ed8152a071793c84f4
Matt Sprague <mesprague@gmail.com>
2016-05-16 20:09:02 +0000

    Birth of the slack puppy

---
+++ puppy.js
@@ -0,0 +1,70 @@
+// Starting code for bot thanks to https://github.com/BeepBoopHQ/starter-node-bot/blob/master/index.js
+
+var Botkit = require('botkit')
+
+// Expect a SLACK_TOKEN environment variable
+var slackToken = process.env.SLACK_TOKEN
+if (!slackToken) {
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
+}
+
+var controller = Botkit.slackbot()
+var bot = controller.spawn({
+  token: slackToken
+})
+
+bot.startRTM(function (err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+})
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "Woof (pants)")
+})
+
+controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+  bot.reply(message, 'Woof. Arf arf (runs in circles)')
+})
+
+controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
+  bot.reply(message, '(Looks at you with a puzzled expression')
+  bot.reply(message, 'WOOF!')
+})
+
+controller.hears('.*', ['mention'], function (bot, message) {
+  bot.reply(message, 'Ruff. Arf (puppy smile). :heart:')
+})
+
+controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+  var help = 'I will respond to the following messages: \n' +
+      '`bot hi` for a simple message.\n' +
+      '`bot attachment` to see a Slack attachment message.\n' +
+      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+      '`bot help` to see this again.'
+  bot.reply(message, help)
+})
+
+controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+  var attachments = [{
+    fallback: text,
+    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+    title: 'Host, deploy and share your bot in seconds.',
+    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+    title_link: 'https://beepboophq.com/',
+    text: text,
+    color: '#7CD197'
+  }]
+
+  bot.reply(message, {
+    attachments: attachments
+  }, function (err, resp) {
+    console.log(err, resp)
+  })
+})
+
+controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'Arf?\nWoof?')
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# lord-barkington
+
+## Overview
+A PuppyBot as a mascot for the Learn to Code at the Denver Library group.
+Cute, loyal, and always happy to see you
+
